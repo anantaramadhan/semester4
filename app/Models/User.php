0195dbfa6db61d8bd4531850547656d589ea3136
ants <anantaramadhan00@gmail.com>
2025-02-27 09:28:55 +0700

    perubahn

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,10 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
+
+    // protected $casts = [
+    //     'email_verified_at' => 'datetime',
+    // ];
+    // fjgh
 }
